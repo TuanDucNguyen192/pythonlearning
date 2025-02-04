7457f7ca85799af4964711684d079e4f2fe79dd0
tuanduc <ntd.1120@gmail.com>
2025-02-04 17:36:27 +0700

    if else basic code

---
+++ if_else_basic.py
@@ -0,0 +1,9 @@
+diem = 0
+diem = input("nhap tuoi ")
+diem = int(diem)
+if diem >= 18 and diem <= 40:
+    print("Ban du dieu kien tham roi")
+elif diem > 40:
+    print("Ban da qua tuoi de tham gia")
+else:
+    print("Ban chua du 18 tuoi de tham gia.")
